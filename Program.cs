4f1fba6abafe859f3a40f394732d5da9c4aad0c0
Yaroslav Matiichuk <9653581@gmail.com>
2022-11-09 23:29:03 +0300

    sfsdf

--- Program.cs
+++ Program.cs
@@ -4,12 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lab9
+namespace Task_1 // Оголошення нового простору імен
 {
-    class Program
+    class Program // Оголошення нового класу
     {
-        static void Main(string[] args)
+        static void Main(string[] args) // Оголошення нового методу
         {
+            int number; // Значення для оператора
+            do
+            {
+
+                Console.Write("Set a value X = ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Set a value Y = ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Set a value Z = ");
+                int z = Convert.ToInt32(Console.ReadLine());
+                number = x; 
+                double a = (Math.Pow(x, 2) + 4) / (Math.Sin(z) + (y / 2));
+                Console.WriteLine($"a = {a}");
+
+                if (x < -10) Console.WriteLine($"X less than -10");
+                if ((x > -10) && (x < -1)) Console.WriteLine($"X less than -1");
+                if ((x > 1) && (x < 10)) Console.WriteLine($"X more for 1");
+                if (x > 10) Console.WriteLine($"X more for 10");
+
+
+                if (a < 0) Console.WriteLine($"The result is negative");
+                if (a > 0) Console.WriteLine($"The result is positive");
+                Console.ReadKey();
+            }
+            while (number != 0);
         }
     }
 }
